e0dd24371cced418af536310cad019764e3f11cb
pavelnagaj <pavel.nagai@mail.ru>
2025-05-01 18:25:02 +0300

    fix build

--- wait-for-it.sh
+++ wait-for-it.sh
@@ -3,14 +3,18 @@
 # Wait for services to be ready
 echo "Waiting for services to be ready..."
 
+# Get the IP address of the db container
+DB_IP=$(getent hosts db | awk '{ print $1 }')
+MINIO_IP=$(getent hosts minio | awk '{ print $1 }')
+
 # Wait for PostgreSQL
-until nc -z db 5432; do
+until nc -z $DB_IP 5432; do
   echo "Waiting for PostgreSQL..."
   sleep 1
 done
 
 # Wait for MinIO
-until nc -z minio 9000; do
+until nc -z $MINIO_IP 9000; do
   echo "Waiting for MinIO..."
   sleep 1
 done
